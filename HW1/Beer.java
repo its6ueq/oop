f7219d91de85730f401a7b3858238cde390dc227
its6ueq <totenhaibang@gmail.com>
2024-04-09 12:59:58 +0700

    new file:   HW1/Beer.java 	deleted:    HW1/exerciseb.java

---
+++ HW1/Beer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println ("Nguyen Duy Hai Bang \t 23020335 \t AI1K68 \t its6ueq \t totenhaibang@gmail.com");
+        for (int i = 9; i > 0; i--)
+            System.out.println (i + " bottles of beer on the wall, " + i + " bottles of beer.\nTake one down, pass it around,");
+        System.out.println ("No more bottles of beer on the wall.\n");
+    }
+}
+
