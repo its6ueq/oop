3caa88a4c4a3684cdae8d81711f42cc367101c9f
its6ueq <totenhaibang@gmail.com>
2024-04-11 21:01:50 +0700

    new file:   HackerRank/Solution.java 	new file:   HackerRank/javaDatatypes.java 	new file:   HackerRank/javaEndOfFile.java 	new file:   HackerRank/javaIfElse.java 	new file:   HackerRank/javaIntToString.java 	new file:   HackerRank/javaLoopsI.java 	new file:   HackerRank/javaLoopsII.java 	new file:   HackerRank/javaOutputFormatting.java 	new file:   HackerRank/javaStaticInitializerBlock.java 	new file:   HackerRank/javaStdinAndStdout.java 	new file:   HackerRank/javaStdinAndStdoutII.java 	new file:   HackerRank/welcomeToJava.java

---
+++ HW1/HackerRank/javaOutputFormatting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println ("================================");
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next ();
+            int x = sc.nextInt ();
+            System.out.print (s1);
+            int si = 15 - s1.length ();
+            for (int j = 0; j < si; j++)
+                System.out.print (" ");
+            if (x < 10) System.out.println ("00" + x);
+            else if (x < 100) System.out.println ("0" + x);
+            else System.out.println (x);
+        }
+        System.out.println ("================================");
+    }
+}
+
+
+
+
